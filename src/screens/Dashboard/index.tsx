ca571695747d5a12187f40cccc362da891f57f5e
Wanderson Chaves <wandersonscpibr@gmail.com>
2023-06-19 21:39:51 -0300

    finished challenges

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useRef, useState } from 'react';
-import { TextInput } from 'react-native';
+import { Alert, TextInput } from 'react-native';
 
 import { Background } from '../../components/Background';
 import { Card } from '../../components/Card';
@@ -16,14 +16,14 @@ import {
   InputField,
   InputButton,
   Icon,
-  RepositoriesList
+  RepositoriesList,
 } from './styles';
 
 type RootStackParamList = {
   Dashboard: undefined;
   Repository: {
     repositoryId: number;
-  }
+  };
 };
 
 type NavigationProps = StackNavigationProp<RootStackParamList, 'Dashboard'>;
@@ -37,22 +37,21 @@ export function Dashboard() {
   const { addRepository, repositories } = useRepositories();
 
   function handleAddRepository() {
-    /**
-     * TODO: 
-     * - call addRepository function sending inputText value;
-     * - clean inputText value.
-     */
+    if (inputText.trim() !== '') {
+      addRepository(inputText);
+      setInputText('');
+    } else {
+      Alert.alert(
+        'Campo de busca vazio',
+        'Por favor, digite o nome do repositório'
+      );
+    }
   }
 
   function handleRepositoryPageNavigation(id: number) {
-    /**
-     * TODO - navigate to the Repository screen sending repository id.
-     * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
-     * navigate(SCREEN NAME, {
-     *  repositoryId: id of the repository
-     * })
-     */
+    navigate('Repository', {
+      repositoryId: id,
+    });
   }
 
   return (
@@ -66,27 +65,19 @@ export function Dashboard() {
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
-              /**
-               * TODO - update inputText value when input text value 
-               * changes:
-               * onChangeText={YOUR CODE HERE}
-               */
+              onChangeText={setInputText}
               onSubmitEditing={handleAddRepository}
-              returnKeyType="send"
+              returnKeyType='send'
               autoCapitalize='none'
               autoCorrect={false}
             />
 
             <InputButton
-              testID="input-button"
+              testID='input-button'
               onPress={handleAddRepository}
-              /**
-               * TODO - ensure to disable button when inputText is 
-               * empty (use disabled prop to this):
-               * disabled={CONDITION HERE}
-               */
+              disabled={inputText === ''}
             >
-              <Icon name="search" size={20} />
+              <Icon name='search' size={20} />
             </InputButton>
           </Input>
         </AddGithubRepo>
@@ -94,7 +85,7 @@ export function Dashboard() {
         <RepositoriesList
           data={repositories}
           showsVerticalScrollIndicator={false}
-          keyExtractor={repository => String(repository.id)}
+          keyExtractor={(repository) => String(repository.id)}
           renderItem={({ item: repository }) => (
             <Card
               key={repository.id}
@@ -102,7 +93,7 @@ export function Dashboard() {
                 id: repository.id,
                 title: repository.full_name,
                 subTitle: repository.description,
-                imageUrl: repository.owner.avatar_url
+                imageUrl: repository.owner.avatar_url,
               }}
               onPress={() => handleRepositoryPageNavigation(repository.id)}
             />
@@ -110,5 +101,5 @@ export function Dashboard() {
         />
       </Container>
     </Background>
-  )
+  );
 }
